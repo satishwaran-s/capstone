0af5c190c844fc5ea2b251dd49b4ab387683c9dd
satish <91818514+satishwaran-s@users.noreply.github.com>
2025-03-05 11:08:33 +0700

    Update display.launch.py

--- src/capstone_description/launch/display.launch.py
+++ src/capstone_description/launch/display.launch.py
@@ -31,6 +31,59 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    # # Launch the controller_manager node
+    # controller_manager_params = {
+    #     'use_sim_time': use_sim_time,
+    #     'robot_description': robot_description_config,
+    #     'controller_manager': os.path.join(get_package_share_directory('capstone_description'), 'config', 'controller.yaml')
+    # }
+    
+    # node_controller_manager = Node(
+    #     package='controller_manager',
+    #     executable='ros2_control_node',
+    #     name='controller_manager',
+    #     output='screen',
+    #     parameters=[controller_manager_params]
+    # )
+
+
+    # # Launch the differential drive controller node
+    # node_diff_drive = Node(
+    #     package='diff_drive_controller',
+    #     executable='diff_drive_controller_node',
+    #     name='diff_drive',
+    #     output='screen',
+    #     parameters=[{
+    #         'cmd_vel_topic': '/cmd_vel',
+    #         'odom_topic': '/odom',
+    #         'wheel_separation': 0.5,
+    #         'wheel_radius': 0.1
+    #     }]
+    # )
+
+    # Static Transform Publisher for map -> odom
+    node_static_transform = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_transform_publisher',
+        output='screen',
+        arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom']
+    )
+
+    # node_robot_state_publisher = Node(
+    #     package='robot_state_publisher',
+    #     executable='robot_state_publisher',
+    #     output='screen',
+    #     parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_description_config}]
+    # )
+
+    slam_node = Node(
+        package= 'slam_toolbox',
+        executable= 'sync_slam_toolbox_node',
+        name= 'slam_toolbox',
+        output= 'screen',
+        parameters= ['/home/satishrpi/capstone_ws/src/capstone_description/config/mapper_params_online_async.yaml']
+    )
 
     # Launch!
     return LaunchDescription([
@@ -43,5 +96,9 @@ def generate_launch_description():
             default_value='true',
             description='Use ros2_control if true'),
 
-        node_robot_state_publisher
+        node_robot_state_publisher,
+        # slam_node
+        # node_static_transform
+        # node_controller_manager,
+        # node_diff_drive
     ])
